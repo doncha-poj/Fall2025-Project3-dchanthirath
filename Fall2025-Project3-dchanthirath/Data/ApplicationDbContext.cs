67bfc6d5aa351432bd998129ac9696405e9c36a0
Donald Chanthirath <chanthirathdonald@gmail.com>
2025-11-12 16:32:48 -0600

    updated applicationdbcontext.cs

--- Fall2025-Project3-dchanthirath/Data/ApplicationDbContext.cs
+++ Fall2025-Project3-dchanthirath/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Fall2025_Project3_dchanthirath.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Fall2025_Project3_dchanthirath.Data
@@ -8,6 +9,27 @@ namespace Fall2025_Project3_dchanthirath.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        // Add your models as DbSets
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<ActorMovie> ActorMovies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Configure the many-to-many relationship
+            builder.Entity<ActorMovie>()
+                .HasOne(am => am.Actor)
+                .WithMany(a => a.ActorMovies)
+                .HasForeignKey(am => am.ActorId);
+
+            builder.Entity<ActorMovie>()
+                .HasOne(am => am.Movie)
+                .WithMany(m => m.ActorMovies)
+                .HasForeignKey(am => am.MovieId);
         }
     }
 }
